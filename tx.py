19181bcd53d29c4a9a848992478422a63e5bed26
sy <sy@mail.js-tech.tw>
2021-06-22 14:59:18 +0800

    fix: 歐洲盃外圍賽移到足球

--- tx.py
+++ tx.py
@@ -1137,7 +1137,7 @@ class TXCrawler:
             game_class = GameType.tennis
         elif game_type is GameType.eSport:
             game_class = GameType.eSport
-        elif '歐洲冠軍' in league or '歐洲盃' in league:
+        elif ('歐洲冠軍' in league or '歐洲盃' in league) and '外圍賽' not in league:
             game_class = GameType.UCL
         elif game_type is GameType.soccer:
             game_class = GameType.soccer
